e836d4e803b143b2e84485a2581b8f0c8d749c7e
Muhammad Syamsul Ma'rif <38209592+xamppsul@users.noreply.github.com>
2026-07-28 12:05:51 +0800

    Supabase connection db (#3)
    * Supabase Init Database Psql
    1. agent skill add on this project
    2. setup connection url for hit database
    
    * Fix:
    1. connection db with supabase
    
    ---------
    
    Co-authored-by: Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>

--- config/database.php
+++ config/database.php
@@ -99,6 +99,11 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        'supabase' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL','postgresql://postgres.hrqwvnftouqimymzjsql:[your_password]@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres'),
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
