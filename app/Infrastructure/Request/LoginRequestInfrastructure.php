43a15530e9b72c42de59cec938dae997f335d4ca
Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>
2026-08-04 15:56:43 +0800

    delete rule password validation: 1. upcase and lowercase minimum 2. input number minimum 3. input special character minimum

--- app/Infrastructure/Request/LoginRequestInfrastructure.php
+++ app/Infrastructure/Request/LoginRequestInfrastructure.php
@@ -15,9 +15,6 @@ class LoginRequestInfrastructure
             'password' => [
                 'required',
                 Password::min(8)
-                    ->mixedCase()
-                    ->numbers()
-                    ->symbols()
             ]
         ]);
     }
