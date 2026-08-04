819e8ca9fcf97646d7db41192bebadcee620d293
Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>
2026-08-04 18:34:35 +0800

    1. add middleware auth api as authorization 2. add cors

---
+++ app/Infrastructure/Middleware/AuthenticatedMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Infrastructure\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticatedMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
+    {
+        // return $request->expectsJson() ? null : route('auth.view.login');
+        if (!Auth::check()) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Session has expired. Please login again.'
+                ], 401);
+            }
+            return redirect()
+                ->route('auth.view.login')
+                ->with('error', 'Silahkan login terlebih dahulu.');
+        }
+
+        return $next($request);
+    }
+}
--- app/Internal/Login/Routes/LoginRoutes.php
+++ app/Internal/Login/Routes/LoginRoutes.php
@@ -5,4 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [LoginHandler::class, 'login'])->name('login');
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/check', function () {
+            return response()->json(['status' => 'ok']);
+        });
+    });
 });
