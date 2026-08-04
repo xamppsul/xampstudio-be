7f6822af05cb7ebbc0e09c6e1723e1628ea5d817
Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>
2026-08-04 19:08:08 +0800

    create endpoint for check session api by user

--- app/Internal/Login/Routes/LoginRoutes.php
+++ app/Internal/Login/Routes/LoginRoutes.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Internal\Login\Handler\LoginHandler;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [LoginHandler::class, 'login'])->name('login');
     Route::middleware('auth:api')->group(function () {
-        Route::get('/check', function () {
-            return response()->json(['status' => 'ok']);
+        Route::get('/check', function (Request $request) {
+            return response()->json(['status' => 'ok', 'data' => $request->user('api')]);
         });
     });
 });
