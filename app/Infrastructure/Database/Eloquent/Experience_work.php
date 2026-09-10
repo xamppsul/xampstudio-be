f1d13befeb826167cd4af16a24ad82e5a8f30c52
Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>
2026-09-10 10:42:46 +0800

    fix orm for custom format response achivement data and techstack

--- app/Infrastructure/Database/Eloquent/Experience_work.php
+++ app/Infrastructure/Database/Eloquent/Experience_work.php
@@ -2,11 +2,14 @@
 
 namespace App\Infrastructure\Database\Eloquent;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Experience_work extends Model
 {
+    protected $appends = ['achivement_list', 'tech_stack_list'];
+    protected $hidden = ['achivement', 'techstack'];
     protected $fillable = [
         'title',
         'start_at',
@@ -33,5 +36,34 @@ class Experience_work extends Model
             ->orderByRaw('CASE WHEN end_at IS NULL THEN 0 ELSE 1 END ASC')
             ->orderBy('end_at', 'desc')
             ->orderBy('start_at', 'desc');
+    }
+
+    /**
+     * eloquent accessor(achivement list) buat ubah format data
+     * (masuk ke key achive untuk mengubah dari list object menjadi list array)
+     **/
+    protected function achivementList(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->relationLoaded('achivement')
+                ? $this->achivement->pluck('achive')
+                : []
+        );
+    }
+
+    /**
+     * eloquent accessor(techstack list) buat ubah format data
+     * (masuk ke key achive untuk mengubah dari list object menjadi list array)
+     **/
+    protected function techStackList(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                $techstack_load = $this->relationLoaded('techstack');
+                if ($techstack_load) {
+                    return $this->techstack->map(fn($item) => $item->tech->tech);
+                }
+            }
+        );
     }
 }
